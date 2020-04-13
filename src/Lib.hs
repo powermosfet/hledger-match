d8285692dc0024459aca191b7ad3225c5ca1deab
Asmund Berge <herrasbe@gmail.com>
2020-04-13 16:18:32 +0200

    Find and print unmatched transactions

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,9 +4,9 @@ module Lib
 
 import Relude
 
-import Hledger (jtxns)
+import Hledger (Transaction(..), MixedAmount, jtxns, pamount, showTransaction)
 import Hledger.Read (readJournal, InputOpts(..), definputopts)
-import Hledger.Query (Query(..), matchesTransaction)
+import Hledger.Query (Query(..), matchesTransaction, matchesPosting)
 import Data.Text.IO (getContents)
 
 inputOptions :: InputOpts
@@ -21,5 +21,36 @@ findUnmatched account = do
         Right j -> do
             let accountQuery = Acct account
             let interestingTxns = filter (matchesTransaction accountQuery) (jtxns j)
-            putStrLn ("Transactions: " <> show (length (interestingTxns)))
-            putStrLn ("Got account " <> account)
+            findMatchRecursively accountQuery interestingTxns
+
+accountAmount :: Query -> Transaction -> Maybe MixedAmount
+accountAmount accountQuery tx =
+    pamount <$> viaNonEmpty head (filter (matchesPosting accountQuery) (tpostings tx))
+
+accountAmountIs :: Query -> MixedAmount -> Transaction -> Bool
+accountAmountIs accountQuery amount tx =
+    accountAmount accountQuery tx == Just amount
+
+removeSingle :: (a -> Bool) -> [a] -> [a]
+removeSingle predicate items =
+    let
+        (before, afterIncludingMatch) = break predicate items
+        after = maybe [] id (viaNonEmpty tail afterIncludingMatch)
+    in
+    before <> after
+
+findMatchRecursively :: Query -> [Transaction] -> IO ()
+findMatchRecursively _ [] = return ()
+findMatchRecursively accountQuery (tx:txs) = do
+    case accountAmount accountQuery tx of
+        Nothing -> 
+            findMatchRecursively accountQuery txs
+
+        Just amount -> do
+            let newTxs = removeSingle (accountAmountIs accountQuery (negate amount)) txs
+            if length newTxs == length txs then do
+                putStrLn (showTransaction tx)
+                putStrLn ""
+                findMatchRecursively accountQuery txs
+            else 
+                findMatchRecursively accountQuery newTxs
